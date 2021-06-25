c3d4b05c44e444cd95cf24b13ebb34fe278cd41c
ashir.muhammad <itxaxhu668@gmail.com>
2021-06-25 22:56:13 +0500

    Updated with role

---
+++ lib/roleselector.dart
@@ -0,0 +1,16 @@
+//re direct from wrapper to choose roles
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:step/student/studenthome.dart';
+import 'package:step/teacher/teacherhome.dart';
+
+class RoleSelector extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return either studenthome/teacherhome/teacheradminhome widgets.
+    return StudentsHomeScreen();
+  }
+}
+
+
---
+++ lib/services/auth.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:step/models/usermodels.dart';
+
+class AuthService {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on User(FirebaseUser)
+  StepUser _userFromFirebaseUser(User user) {
+    return user != null ? StepUser(uid: user.uid) : null;
+  }
+
+  //Auth change user stream
+  Stream<StepUser> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  //sign in with e-mail and password
+  Future signInWithEmailPassword({String email, String password}) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      _userFromFirebaseUser(user);
+      String uid=user.uid;
+      // this is for fetch the role
+      // role side
+      var document = await FirebaseFirestore.instance.doc('users1/$uid').get().then((value) => print(value['role'].toString()));
+
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign in with email and password
+  Future registerWithEmailPassword({String email, String password}) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //Logout with E-mail and password
+  static signout() {
+    return _auth.signOut();
+  }
+  //reister with e-mail and password
+
+}
